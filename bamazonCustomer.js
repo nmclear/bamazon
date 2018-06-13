004a21bf01842c6319b2d7c9840dbd457acf554f
Nick Clear <nickclear22@gmail.com>
2018-06-12 21:35:35 -0700

    added checkOrder function. alerts user if sufficient quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,8 +18,8 @@ var database_name = keys.database_name;
 //========================================================================================================================
 
 var connection = mysql.createConnection({
-    // host: host_name,
-    // port: port_number,
+    host: 'localhost',
+    port: 3306,
     user: "root",
     password: '',
     database: 'bamazonDB'
@@ -41,6 +41,56 @@ function displayAllItems() {
     connection.query(query, function(err, res) {
         for (var i = 0; i < res.length; i++) {
           console.log("ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Price: " + res[i].price);
+        }
+    });
+    buyProduct();
+}
+
+function buyProduct() {
+    inquirer
+      .prompt({
+        name: "item_id",
+        type: "input",
+        message: "What is the ID of the product you would like to buy?",
+      })
+      .then(function(answer) {
+        var buyID = answer.item_id
+        if(isNaN(buyID)){
+            console.log('Please select a valid ID.')
+            buyProduct();
+        } else {
+            inquirer
+            .prompt({
+                name: "units",
+                type: "input",
+                message: "How many units would you like to purchase?",
+            })
+            .then(function(answer) {
+                var buyUnits = answer.units;
+                console.log('item to purchase: ' + buyID);
+                console.log('number of units: ' + buyUnits);
+                var query = 'SELECT item_id, product_name, price FROM products';
+            
+                checkOrder(buyID, buyUnits);
+            });
+        }
+    });
+}
+
+
+function checkOrder(buyID, buyUnits){
+    var query = 'SELECT * FROM products WHERE ?';
+    connection.query(query, {item_id: buyID}, function(err, res) {
+  
+        var result = res[0];
+        var stockUnits = result.stock_quantity;
+
+        console.log(stockUnits + '.. stock units');
+
+        if((stockUnits-buyUnits) < 0){
+            console.log('Insufficient quantity.');
+        } else {
+            console.log('true');
         }
     });
 }
