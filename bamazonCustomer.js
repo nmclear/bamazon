0379d094b16ec123b31832c5b56136ea9ccc0e85
Nick Clear <nickclear22@gmail.com>
2018-06-12 17:53:39 -0700

    added extra files to protect database info.

---
+++ bamazonCustomer.js
@@ -0,0 +1,11 @@
+
+var keys = require("./keys.js");
+
+//========================================================================================================================
+// PRIVATE INFO
+//========================================================================================================================
+
+var port_number = keys.port_number;
+var host_name = keys.host_name;
+var database_password = keys.database.password;
+var database_name = keys.database.name;
---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.host = {
+  host_name: process.env.HOST_NAME,
+  port_number: process.env.PORT_NUMBER,
+};
+
+exports.database = {
+  name: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD
+};
